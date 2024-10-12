7e5427dea57c2e9b1d633bc32a910d44120a390b
Vineet Narayan <vinnar@gmail.com>
2024-10-12 07:01:38 -0400

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'Official Next.js Course Dashboard - Example created by VN',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
 
 export default function RootLayout({
   children,
